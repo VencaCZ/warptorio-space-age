11807234accb4bb55cbd529bf8ff3f75e3ff0262
Asaduzzaman Pavel <contact@iampavel.dev>
2026-09-01 17:37:45 +0600

    Feat: Show ground minimap while in factory/garden

--- settings.lua
+++ settings.lua
@@ -1,4 +1,3 @@
-
 data:extend(
 {
 	{type = "string-setting",name="warptorio_factory-tile",order="11b",
@@ -122,6 +121,9 @@ data:extend(
      default_value = "square",
      allowed_values = {"square","circle","hexagon"},
      order = "a1"
-  }
+  },
+
+  {type = "bool-setting",name="warptorio-ground-minimap",order="a1",
+   setting_type="runtime-per-user",default_value=true}
 }
 )
